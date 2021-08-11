3ce1e3511a106efa47d89b0cf7544b2f461095a9
khetarpal92 <khetarpal92@gmail.com>
2021-08-11 23:07:34 +0530

    Changes By Kanishk

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Arun Yadav,Ankush and Abhishek Aayushi, Kanishk, Kuldeep Singh
+          Edit <code>src/App.js</code> and save to reload. Arun Yadav,Ankush and Abhishek Aayushi, Kanishk Khetarpal, Kuldeep Singh
         </p>
         <a
           className="App-link"
