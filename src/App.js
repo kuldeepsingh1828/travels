9cab919822b123c89f19042acd7832fdf2ca2314
arun yadav <yadavhul@gmail.com>
2021-08-11 22:50:57 +0530

    Changes by arun

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Arun,Ankush and Abhishek Aayushi, Kanishk, Kuldeep
+          Edit <code>src/App.js</code> and save to reload. Arun Yadav,Ankush and Abhishek Aayushi, Kanishk, Kuldeep
         </p>
         <a
           className="App-link"
