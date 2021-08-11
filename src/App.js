2a8303e61171199c0054b555b6a1de6249021842
khetarpal92 <khetarpal92@gmail.com>
2021-08-11 23:35:15 +0530

    Changes By Kanishk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import React from "react";
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Kanishk from "./components/Kanishk";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Arun Yadav,Ankush and Abhishek Aayushi, Kanishk Khetarpal, Kuldeep Singh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. Arun Yadav,Ankush and Abhishek Aayushi, Kanishk Khetarpal, Kuldeep Singh
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/kanishk" component={Kanishk} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Kanishk.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Kanishk() {
+    return (
+        <>
+            <h1>Component by Kanishk</h1>
+        </>
+    );
+}
+
+export default Kanishk;
