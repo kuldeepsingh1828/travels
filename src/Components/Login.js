e2c72de6979a5475ca742368c8436e5aad5a75e9
khetarpal92 <khetarpal92@gmail.com>
2021-08-17 18:17:23 +0530

    Signup Page Push Data into Array

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,53 +1,25 @@
 import React from 'react';
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            email: '',
-            password: ''
-        };
-
-        this.handleChangeUsername = this.handleChangeUsername.bind(this);
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChangeUsername(event) {
-        this.setState({ email: event.target.value });
-    }
-
-    handleChangePassword(event) {
-        this.setState({ password: event.target.value });
-    }
-
-    handleSubmit(event) {
-        console.log(this.state.email);
-        event.preventDefault();
-    }
-    render() {
-        return (
-            <>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="container py-4">
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                <div className="form-group">
-                                    <label>Email</label>
-                                    <input type="text" className="form-control" onChange={this.handleChangeUsername} />
-                                </div>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input type="password" className="form-control" onChange={this.handleChangePassword} />
-                                </div>
-                                <div className="form-group mt-2">
-                                    <button type="submit" className="btn btn-primary">Login</button>
-                                </div>
-                            </div>
+function Login() {
+    return (
+        <>
+            <div className="container py-4">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input name="email" type="email" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="password" type="password" className="form-control" />
+                        </div>
+                        <div className="form-group mt-2">
+                            <button type="submit" className="btn btn-primary">Login</button>
                         </div>
                     </div>
-                </form>
-            </>
-        );
-    }
+                </div>
+            </div>
+        </>
+    );
 }
 export default Login;
